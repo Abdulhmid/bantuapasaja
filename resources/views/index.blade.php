dfcdfbdc83ffdf1ad1b6c367428e944b11c9807b
abdul <abdul.hamid@tlab.co.id>
2016-03-05 00:51:11 +0700

    news landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,426 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="SI BIG Parking ditujukan bagi pemerintah daerah yang memerlukan transparansi laporan dari pelaksana operasional parkir di lapangan">
+    <meta name="author" content="T'Lab Amazing People">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="robots" content="index,nofollow" />
+	<meta name="copyright" content="Copyright ©2016 T'Lab" />
+	<link rel="canonical" href="http://sibigparking.com/" />
+	<meta name="keywords" content="Smart City, Jukir, Parking, Smart Parking, big data" />
+	<title>SI BIG PARKING</title>
+	
+	<link rel="apple-touch-icon" sizes="57x57" href="dist/images/fav/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="dist/images/fav/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="dist/images/fav/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="dist/images/fav/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="dist/images/fav/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="dist/images/fav/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="dist/images/fav/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="dist/images/fav/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="dist/images/fav/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="dist/images/fav/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="dist/images/fav/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="dist/images/fav/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="dist/images/fav/favicon-16x16.png">
+	<link rel="manifest" href="dist/images/fav/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="dist/images/fav/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+	<!-- main style -->
+	<link rel="stylesheet" href="{!! url('') !!}/dist/css/style.css">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="{!! url('') !!}/dist/components/bootstrap-css/css/bootstrap.min.css">
+	<!-- animate -->
+	<link rel="stylesheet" href="{!! url('') !!}/dist/components/animate.css/animate.css">
+	<!-- font-awesome -->
+	<link rel="stylesheet" href="{!! url('') !!}/dist/components/fontawesome/css/font-awesome.css">
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+	<!-- jQuery -->
+	<script type="text/javascript" src="{!! url('') !!}/dist/components/jquery/dist/jquery.js"></script>
+	<!-- bootstrap.js -->
+	<script type="text/javascript" src="{!! url('') !!}/dist/components/bootstrap-css/js/bootstrap.min.js"></script>
+
+
+</head>
+<body data-spy="scroll" data-target=".box-sidebar">
+
+	<!-- navigation -->
+	<nav class="navigation navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<span class="page-scroll"><a class="logo" href="#overview"></a></span>
+					<div class="navbar-header page-scroll">
+		                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		                    <span class="sr-only">Toggle navigation</span>
+		                    <span class="icon-bar"></span>
+		                    <span class="icon-bar"></span>
+		                    <span class="icon-bar"></span>
+		                </button>
+		            </div>
+		            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav">
+							<!-- <li class="hidden "><a href="#overview"></a></li> -->
+							<li class="page-scroll"><a href="#overview">SI BIG PARKING</a></li>
+							<li class="page-scroll"><a href="#how">Mekanisme</a></li>
+							<li class="page-scroll"><a href="#features">Fitur</a></li>
+							<li class="page-scroll"><a href="#portofolio">Portofolio</a></li>
+							<li class="page-scroll"><a href="#contact">Kontak</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+		
+ 
+	<!-- hero header -->
+	<header id="overview" class="content-page">
+		<div class="overview_content">
+		  	<img src="dist/images/sbp_line.png" alt="" class="logo-home wow flipInX">
+		  	<h1 class="wow flipInX" data-wow-delay="0.2s">Ciptakan transparansi parkir di kotamu</h1>
+		  	<h3 class="wow flipInX" data-wow-delay="0.4s">Dapatkan laporan pendapatan, populasi dan pertumbuhan parkir dengan cepat dan mudah</h3>
+		  	<p class="wow flipInX" data-wow-delay="0.6s">
+		  		SI BIG Parking ditujukan bagi pemerintah daerah yang memerlukan transparansi laporan dari pelaksana operasional parkir di lapangan
+		  	</p>
+		</div>
+	</header>
+
+	<!-- how -->
+	<section id="how" class="content-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="heading-title">Bagaimana Mekanisme SI BIG PARKING</h2>
+	 			</div>
+	 			<div class="clearfix"></div>
+	 			 
+ 				<div class="list-how">
+					<div class="col-md-4 col-sm-6">
+						<div class="list-how__content wow fadeInUp">
+							<figure>
+								<img src="dist/images/how/1.png" alt="">
+							</figure>
+							<p>
+								Operator parkir memberikan laporan yang akan di koneksikan ke SI BIG Parking
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="list-how__content wow fadeInUp">
+							<figure>
+								<img src="dist/images/how/2.png" alt="">
+							</figure>
+							<p>
+								SI BIG Parking menyediakan protokol dan akses ke operator parkir dengan API yang sudah di berikan
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="list-how__content wow fadeInUp">
+							<figure>
+								<img src="dist/images/how/3.png" alt="">
+							</figure>
+							<p>
+								Data yang sudah di kirim ke SI BIG Parking akan dapat di akses administrator/pemerintah daerah
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="list-how__content wow fadeInUp">
+							<figure>
+								<img src="dist/images/how/4.png" alt="">
+							</figure>
+							<p>
+								Akses monitoring disajikan sesuai kebutuhan masing-masing administrator/pemerintah daerah
+							</p>
+						</div>
+					</div>
+					<div class="col-md-5 col-md-6">
+						<div class="list-how__content wow fadeInUp">
+							<figure>
+								<img src="dist/images/how/5.png" alt="">
+							</figure>
+							<p>
+								Petugas parkir dapat memeriksa apakah kendaraan parkir sudah terdaftar sebagai parkir legal atau masih ilegal dari SI BIG Parking dalam rangka penegakan Parking enforcement
+							</p>
+						</div>
+					</div>
+ 				</div>
+	 			 
+			</div>
+		</div>
+	</section>
+
+	<!-- feature -->
+	<section id="features" class="content-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="heading-title">Fitur SI BIG PARKING</h2>
+	 			</div>
+	 			<div class="clearfix"></div>
+	 			<div class="box-features">
+	 				<div class="box-features__list on-center">
+	 					<div class="col-md-6 col-sm-6">
+	 						<figure class="box-features__list-cover cover-on-square wow flipInY">
+	 							<img src="dist/images/fitur-01.png" alt="">
+	 						</figure>
+	 						<div class="box-features__meta wow fadeInUp">
+	 							<h2>Data entry dengan API</h2>
+	 							<p>
+	 								Fitur untuk entry data laporan parkir oleh operator
+	 							</p>
+	 						</div>
+	 					</div>
+	 					<div class="col-md-6 col-sm-6 on-center">
+	 						<figure class="box-features__list-cover cover-on-square wow flipInY">
+	 							<img src="dist/images/fitur-02.png" alt="">
+	 						</figure>
+	 						<div class="box-features__meta wow fadeInUp">
+	 							<h2>Manajemen sistem</h2>
+	 							<p>
+	 								Fitur untuk mengatur operator parkir atau pengguna yang terintegrasi dengan SI BIG Parking
+	 							</p>
+	 						</div>
+	 					</div>
+	 				</div>
+	 				<div class="box-features__list">
+	 					<div class="col-md-7 col-sm-7">
+	 						<figure class="box-features__list-cover">
+	 							<img src="dist/images/cover-2.png" alt="" class="dashboard">
+	 						</figure>
+	 					</div>
+	 					<div class="col-md-5 col-sm-5">
+	 						<div class="box-features__meta wow fadeInleft">
+	 							<h2><em>Real time</em> data monitoring</h2>
+	 							<p>
+	 								Fitur untuk memonitoring pendapatan dan mengatahui populasi serta pertumbuhan parkir pada suatu wilayah secara real time
+	 							</p>
+	 							<h3>administrator/pemerintah daerah</h3>
+	 							<ul>
+	 								<li>laporan pendapatan</li>
+	 								<li>laporan jumlah kendaraan</li>
+	 								<li>statistik lama waktu parkir</li>
+	 							</ul>
+	 							<h3>operator</h3>
+	 							<ul>
+	 								<li>statistik lama waktu parkir</li>
+	 								<li>laporan jumlah kendaraan</li>
+	 								<li>laporan pendapatan</li>
+	 							</ul>
+	 						</div>
+	 					</div>
+	 					
+	 				</div>
+ 	 			</div>
+	 		</div>
+	 	</div>
+	</section>
+	
+	<!-- porto -->
+	<section id="portofolio">
+ 		 <div class="container">
+ 		 	<div class="row">
+ 		 		<div class="col-md-6">
+ 		 			<h2 class="wow flipInX">aplikasi</h2>
+ 		 			<h1 class="wow flipInX" data-wow-delay="0.2s">SI BIG PARKING </h1>
+ 		 			<h2 class="wow flipInX" data-wow-delay="0.4s">di kota Bekasi</h2>
+ 		 		</div>
+ 		 		<div class="col-md-6">
+ 		 			<figure class="photo-portofolio">
+ 		 				<img src="dist/images/photo-portofolio.jpg" alt="">
+ 		 			</figure>
+ 		 		</div>
+ 		 	</div>
+ 		 </div>
+	</section>
+	
+	<!-- kontak -->
+	<section id="contact" class="content-page">
+		<div class="box-contact">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="short-info wow flipInX">
+						<div class="icon"><i class="fa fa-phone"></i></div>
+						<h1>+62 274 2870394</h1>
+						<p>
+							jam operasi kami <br>
+							Senin - Jumat, 08:00-16:30 WIB
+						</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="short-info wow flipInX">
+						<div class="icon"><i class="fa fa-envelope"></i></div>
+						<h1>contact@tlab.co.id</h1>
+						<p>Kirim email kepada kami dan kami akan membalas
+							email Anda secepat mungkin.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<label class="wow flipInX">
+				Jl Tanjung No.126 Nitikan, Sorosutan, Umbulharjo,Yogyakarta, Indonesia- 55162<br>
+				<a href="https://www.google.com/maps/place/TLab/@-7.8238375,110.3817487,17z/data=!4m2!3m1!1s0x2e7a57a052ffdae3:0x4b9989538c6097e1" target="_blank">Lihat peta</a>
+			</label>
+ 		</div>
+ 		  	 
+	</section>
+ 
+ 
+	<!-- Plugin JavaScript -->
+    <script src="{!! url('') !!}/dist/js/jquery.easing.min.js"></script>
+    <script src="{!! url('') !!}/dist/js/classie.js"></script>  
+   	<!-- retina.js 
+	<script type="text/javascript" src="dist/js/retina.min.js"></script>-->
+    <script src="{!! url('') !!}/dist/js/wow.min.js"></script>
+    <script type="text/javascript">
+    	$(document).ready(function(){
+
+    		// jQuery for page scrolling feature - requires jQuery Easing plugin
+			 
+			    $('body').on('click', '.page-scroll a', function(event) {
+			        var $anchor = $(this);
+			        $('html, body').stop().animate({
+			            scrollTop: $($anchor.attr('href')).offset().top
+			        }, 1500, 'easeInOutExpo');
+			        event.preventDefault();
+			    });
+
+			    // Closes the Responsive Menu on Menu Item Click
+				$('.navbar-collapse ul li a').click(function() {
+				    $('.navbar-toggle:visible').click();
+				});
+				    
+
+			    // Highlight the top nav as scrolling occurs
+				// $('body').scrollspy({
+				//     target: '.navbar-fixed-top'
+				// })
+
+				$(window).scroll(function (event) {
+			        var sc = $(window).scrollTop();
+			         
+ 			        headerHeight = $('#overview').outerHeight();
+ 			        if(sc > headerHeight - 200) {
+ 			        	$('nav.navigation').addClass('show_up');
+ 			        } else {
+ 			        	$('nav.navigation').removeClass('show_up');
+ 			        }
+			        
+
+			    });
+
+			     
+
+				/*================================================================ 
+			    //smooth
+			    if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+			    window.onmousewheel = document.onmousewheel = wheel;
+			     
+			    function wheel(event) {
+			        var delta = 0;
+			        if (event.wheelDelta) delta = event.wheelDelta / 120;
+			        else if (event.detail) delta = -event.detail / 3;
+			     
+			        handle(delta);
+			        if (event.preventDefault) event.preventDefault();
+			        event.returnValue = false;
+			    }
+
+			    var forDistance = $(window).outerHeight() ;
+			    // var forDistance = 300;
+			    function handle(delta) {
+			        var time = 600; // delay time
+			        var distance = forDistance; // delta point 
+			        // Dom where it will apply 
+			        $('html, body').stop().animate({
+			            scrollTop: $(window).scrollTop() - (distance * delta)
+			        }, time );
+			    }
+
+			    /*================================================================*/
+
+			    // wow
+
+				function animations() {
+					
+					animate = new WOW({
+						boxClass: 'wow',
+						animateClass: 'animated',
+						offset: 100,
+						mobile: false,
+						live: true
+					})
+					
+					animate.init();
+					
+				}
+
+				animations();
+
+				/*// Hide Header on on scroll down
+				var didScroll;
+				var lastScrollTop = 0;
+				var delta = 5;
+				var navbarHeight = $('.navigation').outerHeight();
+
+				$(window).scroll(function(event){
+				    didScroll = true;
+				});
+
+				setInterval(function() {
+				    if (didScroll) {
+				        hasScrolled();
+				        didScroll = false;
+				    }
+				}, 250);
+
+				function hasScrolled() {
+				    var st = $(this).scrollTop();
+				    
+				    // Make sure they scroll more than delta
+				    if(Math.abs(lastScrollTop - st) <= delta)
+				        return;
+				    
+				    // If they scrolled down and are past the navbar, add class .nav-up.
+				    // This is necessary so you never see what is "behind" the navbar.
+				    if (st > lastScrollTop && st > navbarHeight){
+				        // Scroll Down
+				        $('.navigation').removeClass('nav-down').addClass('nav-up');
+				    } else {
+				        // Scroll Up
+				        if(st + $(window).height() < $(document).height()) {
+				            $('.navigation').removeClass('nav-up').addClass('nav-down');
+				        }
+				    }
+				    
+				    lastScrollTop = st;
+				}*/
+
+				// ajax 
+
+ 
+
+			});
+
+
+	</script>
+</body>
+</html>
